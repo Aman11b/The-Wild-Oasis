2b303ae79e7ddb96f1bb8128cff2728656bcecd6
Aman11b <amansingh1100b@gmail.com>
2026-06-06 11:46:08 +0530

    Building the sidebar and Main Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="cabins" element={<Cabins />} />
+            <Route path="bookings" element={<Bookings />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
