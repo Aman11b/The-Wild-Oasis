22e7446bfa39e87186f0e13aac41c1d04be243d7
Aman11b <amansingh1100b@gmail.com>
2026-06-07 10:41:20 +0530

    fetching cabin data

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import supabase from "./supabase";
 
-export async function getCabin() {
+export async function getCabins() {
   const { data: cabins, error } = await supabase.from("cabins").select("*");
 
   if (error) {
