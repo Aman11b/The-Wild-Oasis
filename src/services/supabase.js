e7ce19b42746308ea504c43e7cb3a989f359e0b3
Aman11b <amansingh1100b@gmail.com>
2026-06-09 15:56:26 +0530

    added creds for supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const VITE_SUPABASE_URL = "https://zxrjcorrfilefpdsoypv.supabase.co";
+const VITE_SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_67zr880lgTe-gP7rPrKHTg_bNVknrBn";
+
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseKey = VITE_SUPABASE_PUBLISHABLE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
